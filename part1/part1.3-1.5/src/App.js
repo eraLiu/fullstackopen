af0c611f05b3ccb197a3e916000a70cd50fb71cc
entongliu <era.liu0328@yahoo.com>
2021-11-30 15:51:07 +1300

    step5

--- part1/part1.3-1.5/src/App.js
+++ part1/part1.3-1.5/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Header = (props) => {
   const { course } = props;
+  // console.log(course)
   return (
     <>
       <h1>{course}</h1>
@@ -41,27 +42,29 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
   return (
     <div>
-        <Header course={course} />
-        <Content parts={parts} />
-        <Total num={parts} />
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <Total num={course.parts} />
     </div>
   )
 }
