70442a25d8363b1b8bb140cfb9094ed2f3650044
entongliu <era.liu0328@yahoo.com>
2021-12-01 13:43:59 +1300

    1.6 step1

--- part1/part1.6-1.14/src/App.js
+++ part1/part1.6-1.14/src/App.js
@@ -1,16 +1,44 @@
 import React, { useState } from 'react'
 
+
+const Display = props => <div>{props.text} {props.value}</div>
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const handGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeturalClick = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button handleClick={handGoodClick} text='good'/>
+      <Button handleClick={handleNeturalClick} text='neutral'/>
+      <Button handleClick={handleBadClick} text='bad'/>
+      <h1>statistics</h1>
+      <Display text='good' value={good}/>
+      <Display text='netural' value={neutral}/>
+      <Display text='bad' value={bad}/>
     </div>
   )
 }
+
+const Button = ({handleClick, text}) => 
+  (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+
 
 export default App
