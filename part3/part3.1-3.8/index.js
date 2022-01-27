3d7036325d38bc798efd9e48c01cc92772075f47
entongliu <era.liu0328@yahoo.com>
2022-01-28 11:10:19 +1300

    3.5 step5

--- part3/part3.1-3.8/index.js
+++ part3/part3.1-3.8/index.js
@@ -1,4 +1,4 @@
-const { request } = require('express')
+const { request, response } = require('express')
 const express = require('express')
 const app = express()
 app.use(express.json())
@@ -54,6 +54,28 @@ app.delete('/api/persons/:id', (request, response) =>{
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id != id)
     response.status(204).end()
+}) 
+const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+app.post('/api/persons',(request,response) =>{
+    const body = request.body
+    if(!body.name || !body.number) {
+        return response.status(400).json({
+            error:'content missing'
+        })
+    }
+    const person = {
+        name: body.name,
+        number: body.number,
+        date: new Date(),
+        id:generateId(),
+    }
+    persons = persons.concat(person)
+    response.json(person)
 })
 const PORT = 3001
 app.listen(PORT, () => {
